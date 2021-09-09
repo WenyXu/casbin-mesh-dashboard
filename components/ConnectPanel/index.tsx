834a49e9476dbbbd59e1d8b51b17187c445b8b00
wenyxu <wenymedia@gmail.com>
2021-09-10 05:48:45 +0900

    fix(components/ConnectPanel): improve validation

--- components/ConnectPanel/index.tsx
+++ components/ConnectPanel/index.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import Card from '../utils/Card';
 import FieldWrap from '../utils/Field';
 import { useForm, Controller } from 'react-hook-form';
+import { parseConnectString } from '../../services/client/utils/url';
+import { isError } from '../../services/client/interface';
 export type ConnectPanelProps = {
   loading?: boolean;
   onFinish: (value: Record<'connection', string>) => void | Promise<void>;
@@ -15,7 +17,11 @@ const ActionWrap = styled(FieldWrap)`
 `;
 
 const ConnectPanel = ({ loading, onFinish }: ConnectPanelProps) => {
-  const { control, handleSubmit } = useForm();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
     <Card loading={loading} loadingLabel="connecting">
@@ -24,10 +30,24 @@ const ConnectPanel = ({ loading, onFinish }: ConnectPanelProps) => {
           name="connection"
           control={control}
           defaultValue="mesh://root:root@localhost:4002"
+          rules={{
+            required: true,
+            validate: (value: string) => {
+              console.log('get', value);
+              const result = parseConnectString(value);
+              if (isError(result)) {
+                console.error(result.message);
+                return result.message;
+              }
+              console.log('pass');
+              return true;
+            },
+          }}
           render={({ field }) => (
             <TextField
               placeholder="mesh://root:root@localhost:4002,localhost:4004,localhost:4006"
               label="Casbin Mesh Dashboard"
+              errorMessage={errors?.connection && errors?.connection?.message}
               {...field}
             />
           )}
