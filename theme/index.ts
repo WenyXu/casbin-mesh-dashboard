966f511b60c753c3860dc8f4724af1de88768efb
wenyxu <wenymedia@gmail.com>
2021-09-10 04:34:21 +0900

    feat(theme): define theme

---
+++ theme/index.ts
@@ -0,0 +1,28 @@
+import { createTheme } from '@fluentui/react';
+
+export const Theme = createTheme({
+  palette: {
+    themePrimary: '#443d80',
+    themeLighterAlt: '#f5f5fa',
+    themeLighter: '#d9d7eb',
+    themeLight: '#bab7d9',
+    themeTertiary: '#807bb3',
+    themeSecondary: '#544d8f',
+    themeDarkAlt: '#3d3773',
+    themeDark: '#342f61',
+    themeDarker: '#262247',
+    neutralLighterAlt: '#faf9f8',
+    neutralLighter: '#f3f2f1',
+    neutralLight: '#edebe9',
+    neutralQuaternaryAlt: '#e1dfdd',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c6c4',
+    neutralTertiary: '#a19f9d',
+    neutralSecondary: '#605e5c',
+    neutralPrimaryAlt: '#3b3a39',
+    neutralPrimary: '#323130',
+    neutralDark: '#201f1e',
+    black: '#000000',
+    white: '#ffffff',
+  },
+});
